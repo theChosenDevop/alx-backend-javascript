2cace3f54a58b5c4ee03ad92d472dcad95ef862e
theChosenDevop <tobbey.adesanya@gmail.com>
2024-04-24 23:12:09 +0000

    ES6 Basics

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,6 @@
+export default function createReportObject(employeesList) {
+  const allEmployees = { ...employeesList };
+
+  const getNumberOfDepartments = () => Object.keys(allEmployees).length;
+  return { allEmployees, getNumberOfDepartments };
+}
--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,6 +1,3 @@
 export default function returnHowManyArguments(...args) {
-	for (const i of args)
-		let x =0;
-		x += 1;
-	return (x);
+  return args.length;
 }
