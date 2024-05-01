667c9633c8aae57402e6d458cb6fd6ded6caaeed
theChosenDevop <tobbey.adesanya@gmail.com>
2024-05-01 04:55:39 +0000

    update

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -5,7 +5,7 @@
  * @returns A string of all the set values that start with a specific string(stratString)
  */
 export default function cleanSet(set, startString) {
-  if (startString === undefined || startString === '') {
+  if (startString === undefined || startString.length === 0) {
     return '';
   }
 
