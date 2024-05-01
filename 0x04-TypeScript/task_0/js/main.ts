9f757939e64c79bcb4fefb209ae007abc6ebe717
theChosenDevop <tobbey.adesanya@gmail.com>
2024-05-01 21:58:57 +0000

    initial setup

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,22 @@
+interface Student {
+  firstName: string,
+  lastName: string,
+  age: number,
+  location: string
+  };
+
+const firstStudent: Student = {
+  firstName: 'Oluwatosin',
+  lastName: 'Akanji',
+  age: 23,
+  location: 'Ogun'
+  };
+
+const secondStudent: Student = {
+  firstName: 'Okon',
+  lastName: 'Imudiahlon',
+  age: 43,
+  location: 'Edo'
+  };
+
+const studentsList = [firstStudent, secondStudent];
