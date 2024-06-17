012b83c0e43b7291e0fd454e32271e0e491b1683
theChosenDevop <tobbey.adesanya@gmail.com>
2024-06-17 23:38:04 +0000

    interactive terminal input or not

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,30 @@
+// reads stabdard input
+const displayMessage = require('./0-console');
+
+const msg = 'Welcome to Holberton School, what is your name?';
+
+const endMsg = 'This important software is now closing';
+
+displayMessage(msg);
+
+process.stdin.setEncoding('utf8');
+if (process.stdin.isTTY) {
+  process.stdin.on('readable', () => {
+    const userInput = process.stdin.read();
+    if (userInput !== null) {
+      const input = userInput.trim();
+      displayMessage(`Your name is: ${input}`);
+      process.stdin.end();
+    }
+  });
+} else {
+  let input = '';
+  process.stdin.on('data', (data) => {
+    input += data;
+  });
+  process.stdin.on('end', (end) => {
+    input = input.trim();
+    displayMessage(`Your name is: ${input}`);
+    displayMessage(endMsg);
+  });
+}
