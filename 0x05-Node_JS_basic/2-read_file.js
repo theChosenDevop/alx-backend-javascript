1dceac25a95e9407ec6b06776c33b1d49de4a64a
theChosenDevop <tobbey.adesanya@gmail.com>
2024-06-18 22:20:41 +0000

    read file synchronously

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,29 +1,28 @@
 //  Reading a file synchronously with Node JS
 const fs = require('fs');
 
-function countStudents(file) {
+function countStudents (file) {
   let data = fs.readFileSync(file, 'utf-8');
-  let student_data = {};
-  data = data.split('\n')
+  const studentData = {};
+  data = data.split('\n');
   data.shift();
   // console.log(data);
   data.pop();
   //  console.log(data.length);
-  let first_name, last_name, field, age;
   data.forEach((value) => {
-    const [first_name, last_name, age, field] = value.split(',');
-      if (first_name && last_name && age && field) {
-        if (!student_data[field]) {
-          student_data[field] = [];
-	}
-        student_data[field].push(first_name);
+    const [firstName, lastName, age, field] = value.split(',');
+    if (firstName && lastName && age && field) {
+      if (!studentData[field]) {
+        studentData[field] = [];
       }
+      studentData[field].push(firstName);
+    }
   });
   // console.log(student_data);
-  console.log("Number of students: " + data.length);
-for (const [key, value] of Object.entries(student_data)) {
-  console.log(`Number of students in ${key}: ${value.length}, List: ${value.join(', ')}`);
-}
+  console.log('Number of students: ' + data.length);
+  for (const [key, value] of Object.entries(studentData)) {
+    console.log(`Number of students in ${key}: ${value.length}, List: ${value.join(', ')}`);
+  }
 }
 
 module.exports = countStudents;
