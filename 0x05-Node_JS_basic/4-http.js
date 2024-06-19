0f770cbe4ec152e2b60384715e031b4a6eb84205
theChosenDevop <tobbey.adesanya@gmail.com>
2024-06-19 02:50:26 +0000

    set server response header

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -2,9 +2,14 @@
 const http = require('http');
 
 const app = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+
   res.end('Hello Holberton School!');
 });
+
 const port = 1245;
 app.listen(port, () => {
   console.log(`Server is listening on ${port}`);
 });
+
+module.exports = app;
