e0f76d1b9c9ca707ab2ed4479a41c466f0f84186
theChosenDevop <tobbey.adesanya@gmail.com>
2024-07-29 22:13:23 +0000

    updated test cases

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -10,4 +10,12 @@ describe('calculateNumber', () => {
   it('should return 5 when inputs are 1 and 3.7', () => {
     assert.strictEqual(calculateNumber(1, 3.7), 5);
   });
+
+  it('should return 5 when inputs are 1.2 and 3.7', () => {
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+  });
+
+  it('should return 6 when inputs are 1.5 and 3.7', () => {
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  });
 });
