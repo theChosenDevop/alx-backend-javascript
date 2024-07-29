c3c8d1a858f216dca623bea228b295d30b8ccd93
theChosenDevop <tobbey.adesanya@gmail.com>
2024-07-29 22:04:41 +0000

    assert funtion round-up float and get sum

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,5 +2,12 @@ const calculateNumber = require('./0-calcul');
 const assert = require('assert');
 
 // Test Cases
-assert.strictEqual(calculateNumber(1, 3), 4, '1 + 3 should return 4');
-assert.strictEqual(calculateNumber(1.2, 3.7), 5, '1.2 + 3.7 should return 5');
+describe('calculateNumber', () => {
+  it('should return 4 when inputs are 1 and 3', () => {
+    assert.strictEqual(calculateNumber(1, 3), 4);
+});
+
+  it('should return 5 when inputs are 1 and 3.7', () => {
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
+  });
+});
