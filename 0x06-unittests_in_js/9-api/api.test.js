7644a3f1c2ca06144cf35debd9c18d49c397259f
theChosenDevop <tobbey.adesanya@gmail.com>
2024-07-31 16:44:01 +0000

    Regex integration testing

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -3,41 +3,40 @@ const { expect } = chai;
 const request = require('request');
 
 describe('Index page', () => {
-  it('localhost response', (done) => {
+  it('response', (done) => {
     try {
       request('http://localhost:7865/', (err, res, body) => {
         if (err) throw err;
-	expect(body).to.be.equal('Welcome to the payment system');
-	expect(res.statusCode).to.equal(200);
-	done();
+        expect(body).to.equal('Welcome to the payment system');
+        expect(res.statusCode).to.equal(200);
+        done();
       });
-    } catch(err) {
+    } catch (err) {
       done(err);
     }
   });
-
-  describe('Cart', () => {
-    it('localhost response', (done) => {
+  describe('cart', () => {
+    it('response', (done) => {
       try {
-        request('http://localhost:7865/cart/23', (err, res, body) => {
+        request('http://localhost:7865/cart/7', (err, res, body) => {
           if (err) throw err;
-	  expect(body).to.equal('Payment methods for cart 23');
-	  expect(res.statusCode).to.equal(200);
-	  done();
+          expect(body).to.equal('Payment methods for cart 7');
+          expect(res.statusCode).to.equal(200);
+          done();
         });
-      } catch (err){
-        done(err);
+      } catch (error) {
+        done(error);
       }
-    }
+    });
     it('not number', (done) => {
-      try{
-        request('http://localhost:7865/cart/theChosenDevop', (err, res, body) => {
-          if (err) throw err;
-	  expect(res.statusCode).to.equal(404);
-	  done();
+      try {
+        request('http://localhost:7865/cart/theChosenDevop', (err, res) => {
+          if (err) throw error;
+          expect(res.statusCode).to.equal(404);
+          done();
         });
-      } catch(err){
-        done(err)
+      } catch (err) {
+        done(err);
       }
     });
   });
